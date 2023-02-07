0b63ea213a897871b8cd6b2035187866d36c237a
bgabitobrodsky <bernardogabito@gmail.com>
2023-02-07 19:05:42 -0300

    instalación de paquetes Calendar y date-nfs

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,45 @@
-import React from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css"
+
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
 import { Navbar } from "../";
+
+const locales = {
+	"en-US": enUS,
+};
+const localizer = dateFnsLocalizer({
+	format,
+	parse,
+	startOfWeek,
+	getDay,
+	locales,
+});
+
+const events = [{
+    title: 'asd',
+    notes: 'asdasdas',
+    start: new Date(),
+    end: addHours( new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+        _id: '123',
+        name: 'Fernando'
+    }
+}]
 
 export const CalendarPage = () => {
 	return (
 		<>
 			<Navbar />
+			<Calendar
+				localizer={localizer}
+				events={events}
+				startAccessor="start"
+				endAccessor="end"
+				style={{ height: 'calc( 100vh - 80px)' }}
+			/>
 		</>
 	);
 };
