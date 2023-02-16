29b71bf19825024dfea96b39188c16603bb2e4d3
bgabitobrodsky <bernardogabito@gmail.com>
2023-02-16 10:23:06 -0300

    react redux config

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './router'
+import { store } from './store'
 
 export const CalendarApp = () => {
     return (
-        <BrowserRouter>
-            <AppRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <AppRouter>
 
-            </AppRouter>
-        </BrowserRouter>
+                </AppRouter>
+            </BrowserRouter>
+        </Provider>
     )
 }
