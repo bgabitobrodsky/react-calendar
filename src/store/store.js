0806afb0e9e8ed324207fc9f63d8c774984dfe40
bgabitobrodsky <bernardogabito@gmail.com>
2023-02-17 12:08:54 -0300

    fixing date error

--- src/store/store.js
+++ src/store/store.js
@@ -5,5 +5,8 @@ export const store = configureStore({
     reducer:{
         calendar: calendarSlice.reducer,
         ui: uiSlice.reducer,
-    }
+    },
+    middleware: ( getDefaultMiddleware ) => getDefaultMiddleware({
+        serializableCheck: false
+    })
 })
